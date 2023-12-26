5642dc8caa380a0530fa6ef286518d513ee6fbf6
kanxiaoxi <wzxjachinwu@gmail.com>
2023-12-26 17:17:22 +0800

    try tauri

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,85 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use serde::{ Serialize, Deserialize };
+use tauri::Manager;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .setup(|app| {
+            let _id = app.listen_global("front-to-back", |event| {
+                println!(
+                    "got front-to-back event with payload: {:?}",
+                    event.payload().unwrap()
+                )
+            });
+            
+            let app_handle = app.handle();
+            std::thread::spawn(move || loop {
+               app_handle.emit_all("back-to-front", "ping frontend".to_string()).unwrap();
+                std::thread::sleep(std::time::Duration::from_secs(1));
+            });
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            simple_command,
+            command_with_message,
+            command_with_object,
+            command_with_error, 
+            async_command
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn simple_command() {
+    println!("I was invoked from JS!");
+}
+
+#[tauri::command]
+fn command_with_message(message: String) -> String {
+    format!("hello {}", message)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MyMessage {
+    field_str: String,
+    field_u32: u32,
+}
+
+
+#[tauri::command]
+fn command_with_object(message: MyMessage) -> MyMessage {
+    let MyMessage {
+        field_str,
+        field_u32,
+    } = message;
+
+    MyMessage {
+        field_str: format!("hello {}", field_str),
+        field_u32: field_u32 + 1,
+    }
+}
+
+#[tauri::command]
+fn command_with_error(arg: u32) -> Result<String, String> {
+    if arg % 2 == 0 {
+        Ok(format!("even value {}", arg))
+    } else {
+        Err(format!("odd value {}", arg))
+    }
+}
+
+#[tauri::command]
+async fn async_command(arg: u32) -> String {
+    format!("hello {}", arg).into()
+}
